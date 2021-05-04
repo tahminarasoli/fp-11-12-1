c9f2b6c48894e6a7e9ad5e28743a03d38327ee71
raoul <jraoul2002@gmail.com>
2021-05-04 23:39:22 +0200

    welcome message

--- api/routes/home.js
+++ api/routes/home.js
@@ -6,7 +6,7 @@ const app = express();
 * Just a simple test endpoint to demo how to test with Jest
 **/
 app.get('/home', function(req, res) {
-  res.send('Welcome!');
+  res.send('HELLO WORLD FROM GROUP 1');
 });
 
 module.exports = app;
