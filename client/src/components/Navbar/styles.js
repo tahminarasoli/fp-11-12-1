31f4401a6ee5d96b1e574e1aea22d4edeb51e865
KrystynaMil <kristina-manager@hotmail.com>
2021-05-27 15:27:35 +0200

    bugs fix

--- client/src/components/Navbar/styles.js
+++ client/src/components/Navbar/styles.js
@@ -105,48 +105,4 @@ export default makeStyles((theme) => ({
 
 }));
 
-  button: {
-    position: "relative",
-    width: "220px",
-    height: "60px",
-    background: "linear-gradient(90deg, #70D3FC 0%, #346AD1 100%)",
-    boxShadow:
-      "box-shadow: 10px 10px 20px rgba(38, 80, 127, 0.2), -10px -10px 20px rgba(38, 80, 127, 0.2), 10px -10px 20px rgba(90, 188, 255, 0.9), -10px 10px 25px rgba(38, 80, 127, 0.9), inset -1px 1px 2px rgba(90, 188, 255, 0.3), inset 1px -1px 2px rgba(38, 80, 127, 0.5)",
-    borderRadius: 30,
-  },
-  toolbar: {
-    display: "flex",
-    justifyContent: "flex-end",
-    width: "400px",
-    position:"relative"
-  },
-  profile: {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    width: "300px",
-  },
-  userName: {
-    display: "flex",
-    alignItems: "center",
-  },
-  brandContainer: {
-    display: "flex",
-    alignItems: "center",
-  },
-  purple: {
-    color: theme.palette.getContrastText(deepPurple[500]),
-    backgroundColor: "red",
-
-  },
-  profile_button: {
-    width: "220px",
-    height: "60px",
-    color: "#5B5B61",
-    background: "linear-gradient(135deg, #BBD2DF 0%, #CFE8F7 100%)",
-    boxShadow:
-      "2px 2px 4px rgba(118, 133, 141, 0.2), -2px -2px 4px rgba(118, 133, 141, 0.2), 2px -2px 4px rgba(255, 255, 255, 0.9), -2px 2px 5px rgba(118, 133, 141, 0.9), inset -1px 1px 2px rgba(255, 255, 255, 0.3), inset 1px -1px 2px rgba(118, 133, 141, 0.5)",
-    borderRadius: 30,
-  },
-}));
-
+ 
