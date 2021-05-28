0e207e45f18c564393fb0b809d40539aec8e9f49
Raoul Junang <63600677+jraoul2002@users.noreply.github.com>
2021-05-28 16:03:48 +0200

    Update Showcase.js

--- client/src/components/Showcase/Showcase.js
+++ client/src/components/Showcase/Showcase.js
@@ -22,7 +22,7 @@ const Showcase = () => {
         <ReactPlayer
         className ={classes.reactplayer}
         url="https://youtu.be/o9NAY3BrV7o"
-        width = "330px"
+        width = "328px"
         height = "300px"
         align='center'
       
